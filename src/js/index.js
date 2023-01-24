43b62839fcc505bfca5ca9d81b08b1a6b254c212
Lygia Cipriano <121272073+lygiaclc@users.noreply.github.com>
2023-01-24 13:30:35 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,24 @@
+console.log('mostrar o document', document);
+
+console.log(document.querySelector(".botao-trailer"));
+
+const botaoTrailer = document.querySelector(".botao-trailer");
+const botaoFecharModal = document.querySelector(".fechar-modal");
+const video = document.getElementById("video");
+const modal = document.querySelector(".modal");
+const linkDoVideo = video.src;
+
+function alternarModal(){
+    modal.classList.toggle("aberto");
+}
+
+botaoTrailer.addEventListener("click", () => {
+    alternarModal();
+    video.setAttribute("src", linkDoVideo);
+})
+
+botaoFecharModal.addEventListener("click", () => {
+    alternarModal();
+    video.setAttribute("src", "");
+});
+
